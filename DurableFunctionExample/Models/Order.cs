509c711cbbf61b4d0e58562a791f17edce170ab5
farra0o <82480181+farra0o@users.noreply.github.com>
2025-09-24 18:01:19 -0600

    ClasesReordenadas

--- DurableFunctionExample/Models/Order.cs
+++ DurableFunctionExample/Models/Order.cs
@@ -5,23 +5,43 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DurableFunctionExample.Models
 {
-    [Table("Orders")]
+    [Table("Order")]
     public  class Order
     {
         [Key]
         public int OrderId { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public decimal TotalAmount { get; set; }
-        public int Status { get; set; }
-
-        // FK
         [ForeignKey("User")]
         public int UserId { get; set; }
-        public User User { get; set; }
-         
+        public ICollection<ItemOrder> Items { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public decimal TotalPrice => Items.Sum(item => item.Cantidad * item.Item.Price);
+        public int Status { get; set; }
+        public int PaymentStatus { get; set; }
 
-        // Relaciones
-        public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public class UserD
+        {
+            [Key]
+            public int Id { get; set; }
+            public string Correo { get; set; }
+            public string Nombre { get; set; }
+        }
+        public class Item
+        {
+            [Key]
+            public int ItemId { get; set; }
+            public string ItemName { get; set; }
+            public decimal Price { get; set; }
+            public int ItemStock { get; set; }
+        }
+
+        public class ItemOrder
+        {
+            [Key]
+            public int ItemOrderId { get; set; }
+            public Item Item { get; set; }
+            public string State { get; set; } 
+            public int Cantidad { get; set; }
+        }
     }
 }
